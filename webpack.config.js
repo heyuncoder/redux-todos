23f3975bd345251205a429ff67ede0fea61e7f2e
heyun <heyuncoder@gmail.com>
2016-09-05 23:52:13 +0800

    修改babelrc 添加webpack loader for babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
+var path = require('path');
 module.exports = {
     entry: "./index.js",
     output: {
         path: __dirname,
-        publicPath: "/assets/",
+        //publicPath: "/assets/",
         filename: "bundle.js"
     },
     module: {
-        loaders: [
-            { test: /\.css$/, loader: "style!css" }
-        ]
+        // preLoaders: [{
+        //     test: /\.jsx?$/,
+        //     exclude: /node_modules/,
+        //     loader: 'eslint-loader'
+        // }],
+        loaders: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel', // 'babel-loader' is also a legal name to reference
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
     }
 };
