b73b87d521e55edf2768a2331dfb450c2a9d7fc6
heyun <heyuncoder@gmail.com>
2016-09-08 23:59:33 +0800

    添加Item状态

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,14 @@
 import React,{ Component,PropTypes } from 'react'
 
 export default class Header extends Component {
+    handleKeyup(e){
+        console.log('handleKeyup');
+    }
     render() {
         return ( 
             <header className="header"> 
                 <h1>Todos</h1>
-                <input className="new-todo" placeholder="想要做点什么?" value="" />
+                <input className="new-todo" placeholder="想要做点什么?" onKeyup={this.handleKeyup}/>
             </header>
         )
     }
--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,13 +1,25 @@
 import React, { Component, PropTypes } from 'react'
 
 export default class TodoItem extends Component {
+
+    state = {
+        completed: false
+    };
+
+    handleClick(){
+        console.log('test');
+        this.setState({
+            completed: !this.state.completed
+        });
+    }
+
     render(){
         return (
-            <li className="completed">
+            <li className="{this.state?'completed':''}">
                 <div className="view" >
-                    <input className="toggle" type="checkbox" />
+                    <input className="toggle" type="checkbox" onclick="this.handleClick" />
                     <label>{this.props.children}</label>
-                    <button className="destroy" ></button>
+                    <button className="destroy" >&nbsp;</button>
                 </div>
                 <input className="edit" value="{this.props.children}" />
             </li>
