53f021ff46d26be3daadf7e1429ea0b892a38f10
heyun <heyuncoder@gmail.com>
2016-09-11 23:19:51 +0800

    nothing

--- components/TodoList.js
+++ components/TodoList.js
@@ -10,22 +10,6 @@ export default class Header extends Component {
                     <TodoItem>test1</TodoItem>
                     <TodoItem>test2</TodoItem>
                     <TodoItem>test3</TodoItem>
-                    <li className="completed">
-                        <div className="view" >
-                            <input className="toggle" type="checkbox" />
-                            <label >d</label>
-                            <button className="destroy" ></button>
-                        </div>
-                        <input className="edit" value="d" />
-                    </li>
-                    <li className="" >
-                        <div className="view" >
-                            <input className="toggle" type="checkbox" />
-                            <label >fgh</label>
-                            <button className="destroy" ></button>
-                        </div>
-                        <input className="edit" value="fgh"/>
-                    </li>
                 </ul>
             </section>
         )
